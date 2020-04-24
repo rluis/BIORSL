3711b44160b37760d275690011dac42c8d2dc65f
rluis <ruisergioluis@gmail.com>
2020-04-24 16:20:02 +0100

    setup.py file Added for version 0.0.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='BIORSL',
+    version='0.0.1',
+    packages=['bedEntry', 'bedEntry.test', 'bedContainer', 'bedContainer.testing'],
+    url='https://github.com/rluis/BIORSL',
+    license='GPL-3.0',
+    author='rluis',
+    author_email='ruisergioluis@gmail.com',
+    description='A collection of python modules to computational biology analyses'
+)
