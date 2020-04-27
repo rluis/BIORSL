66fdcfad2099a9ed65c873bafdeee7ec6e363639
rluis <ruisergioluis@gmail.com>
2020-04-27 13:13:05 +0100

    Updated version from 0.0.3 to 0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='BIORSL',
-    version='0.0.3',
+    version='0.0.4',
     packages=['bedEntry', 'bedContainer'],
     url='https://github.com/rluis/BIORSL',
     license='GPL-3.0',
